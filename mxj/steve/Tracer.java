ead3c48791c477d5a6796fdc58ec94d151bde6ff
jcalvert <jcalvert@46ce05f2-5dfb-11de-8dc1-e798e446b60a>
2009-08-13 21:08:01 +0000

    x and y in [-1, 1]  not [0, 1]

--- mxj/steve/Tracer.java
+++ mxj/steve/Tracer.java
@@ -9,150 +9,149 @@ import com.cycling74.msp.MSPPerformer;
 import com.cycling74.msp.MSPSignal;
 
 public abstract class Tracer extends MSPPerformer {
-	JitterMatrix jm = new JitterMatrix();
+    JitterMatrix jm = new JitterMatrix();
 
-	protected static final int[][] nbrs = new int[][] { { 1, 0 }, { 1, -1 },
-			{ 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 } };
+    protected static final int[][] nbrs = new int[][] { { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 },
+            { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 } };
 
-	List<List<Coord>> readTraces = new ArrayList<List<Coord>>();
-	List<List<Coord>> writeEdges = new ArrayList<List<Coord>>();
+    List<List<Coord>> readTraces = new ArrayList<List<Coord>>();
+    List<List<Coord>> writeEdges = new ArrayList<List<Coord>>();
 
-	protected Tracer() {
-		declareInlets(new int[] { DataTypes.ALL });
-		declareOutlets(new int[] { SIGNAL, SIGNAL, SIGNAL });
+    protected Tracer() {
+        declareInlets(new int[] { DataTypes.ALL });
+        declareOutlets(new int[] { SIGNAL, SIGNAL, SIGNAL });
 
-		setInletAssist(INLET_ASSIST);
-		setOutletAssist(OUTLET_ASSIST);
-		declareAttribute("spp");
-		List<Coord> initiEdge = new ArrayList<Coord>();
-		initiEdge.add(new Coord(0,0));
-		readTraces.add(initiEdge);
-		advanceFrame();
-	}
+        setInletAssist(INLET_ASSIST);
+        setOutletAssist(OUTLET_ASSIST);
+        declareAttribute("spp");
+        List<Coord> initiEdge = new ArrayList<Coord>();
+        initiEdge.add(new Coord(0, 0));
+        readTraces.add(initiEdge);
+        advanceFrame();
+    }
 
-	protected float scale;
+    protected float scale;
 
-	private int edgeIndex;
-	private int pointIndex;
-	private int spp = 10;
+    private int edgeIndex;
+    private int pointIndex;
+    private final int spp = 10;
 
-	private int stallIndex;
-	private boolean ready = true;
+    private int stallIndex;
+    private boolean ready = true;
 
-	public void jit_matrix(String s) {
-		if (ready) {
-			jm.frommatrix(s);
-			int dim[] = jm.getDim();
-			int[] data = new int[dim[0] * dim[1]];
-			scale = Math.max(dim[0], dim[1]);
-			jm.copyMatrixToArray(data);
-			ready = false;
-			getTraces(dim[0], dim[1], data);
-			advanceFrame();
-			ready = true;
-		}
-	}
+    public void jit_matrix(String s) {
+        if (ready) {
+            jm.frommatrix(s);
+            int dim[] = jm.getDim();
+            int[] data = new int[dim[0] * dim[1]];
+            scale = Math.max(dim[0], dim[1]);
+            jm.copyMatrixToArray(data);
+            ready = false;
+            getTraces(dim[0], dim[1], data);
+            advanceFrame();
+            ready = true;
+        }
+    }
 
-	private void advanceFrame() {
-		// post("advancing frame with this many edges: " + readEdges.size());
-		writeEdges = readTraces;
-		resetIndexes();
-		readTraces = new ArrayList<List<Coord>>();
-	}
+    private void advanceFrame() {
+        // post("advancing frame with this many edges: " + readEdges.size());
+        writeEdges = readTraces;
+        resetIndexes();
+        readTraces = new ArrayList<List<Coord>>();
+    }
 
-	private void resetIndexes() {
-		this.edgeIndex = 0;
-		this.pointIndex = 0;
-		this.stallIndex = 0;
-	}
+    private void resetIndexes() {
+        this.edgeIndex = 0;
+        this.pointIndex = 0;
+        this.stallIndex = 0;
+    }
 
-	protected abstract void getTraces(int w, int h, int[] data);
-	
-	protected int getPixel(int[] data, int w, int i, int j) {
-		return data[i + j * w];
-	}
+    protected abstract void getTraces(int w, int h, int[] data);
 
-	protected class Coord {
-		int x, y;
+    protected int getPixel(int[] data, int w, int i, int j) {
+        return data[i + j * w];
+    }
 
-		Coord(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
+    protected class Coord {
+        int x, y;
 
-		@Override
-		public boolean equals(Object obj) {
-			if (obj instanceof Coord) {
-				if (((Coord) obj).x == x && ((Coord) obj).y == y) {
-					return true;
-				}
-			}
-			return false;
-		}
+        Coord(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
 
-		@Override
-		public int hashCode() {
-			return x + y * 640;
-		}
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Coord) {
+                if (((Coord) obj).x == x && ((Coord) obj).y == y) {
+                    return true;
+                }
+            }
+            return false;
+        }
 
-		@Override
-		public String toString() {
-			return "(" + x + ", " + y + ")";
-		}
-		
-		public int intDist(Coord c) {
-			int dx = x - c.x;
-			int dy = y - c.y;
-			return (int) Math.min(Math.sqrt(dx * dx + dy * dy), 1);
-		}
+        @Override
+        public int hashCode() {
+            return x + y * 640;
+        }
 
-	}
+        @Override
+        public String toString() {
+            return "(" + x + ", " + y + ")";
+        }
 
-	// / MAX Stuff
-	private static final String[] INLET_ASSIST = new String[] { "messages in" };
+        public int intDist(Coord c) {
+            int dx = x - c.x;
+            int dy = y - c.y;
+            return (int) Math.min(Math.sqrt(dx * dx + dy * dy), 1);
+        }
 
-	private static final String[] OUTLET_ASSIST = new String[] { "x out",
-			"y out", "blanking out" };
+    }
 
-	@Override
-	public void perform(MSPSignal[] in, MSPSignal[] out) {
-//		post("performing! " + stallIndex + ", " + pointIndex + ", " + edgeIndex);
-		for (int i = 0; i < out[0].vec.length; i++) {
-			try {
-				if (stallIndex >= spp) {
-					stallIndex = 0;
-					pointIndex++;
-				} else {
-					stallIndex++;
-				}
-				if (pointIndex >= this.writeEdges.get(edgeIndex).size()) {
-					edgeIndex++;
-					pointIndex = 0;
-					stallIndex = 0;
-				}
-				if (edgeIndex >= this.writeEdges.size()) {
-					edgeIndex = 0;
-					pointIndex = 0;
-					stallIndex = 0;
-				}
-				out[0].vec[i] = (float) (this.writeEdges.get(edgeIndex).get(
-						pointIndex).x * (1.0 / scale));
-				out[1].vec[i] = (float) (this.writeEdges.get(edgeIndex).get(
-						pointIndex).y * (1.0 / scale));
-				out[2].vec[i] = (float) (blank(pointIndex));
-			} catch (Exception e) {
-				// OOB and NPE possible cos of race condition
-				// post("exception:" + e.getMessage());
-				// post(edgeIndex + ": " + pointIndex + ": " + stallIndex);
-				resetIndexes();
-			}
-		}
-	}
+    // / MAX Stuff
+    private static final String[] INLET_ASSIST = new String[] { "messages in" };
 
-	private static final int LAS_OFF = 1;
-	private static final int LAS_ON = 0;
+    private static final String[] OUTLET_ASSIST = new String[] { "x out", "y out", "blanking out" };
 
-	private float blank(int pointIndex) {
-		return pointIndex == 0 ? LAS_OFF : LAS_ON;
-	}
+    @Override
+    public void perform(MSPSignal[] in, MSPSignal[] out) {
+        // post("performing! " + stallIndex + ", " + pointIndex + ", " + edgeIndex);
+        for (int i = 0; i < out[0].vec.length; i++) {
+            try {
+                if (stallIndex >= spp) {
+                    stallIndex = 0;
+                    pointIndex++;
+                } else {
+                    stallIndex++;
+                }
+                if (pointIndex >= this.writeEdges.get(edgeIndex).size()) {
+                    edgeIndex++;
+                    pointIndex = 0;
+                    stallIndex = 0;
+                }
+                if (edgeIndex >= this.writeEdges.size()) {
+                    edgeIndex = 0;
+                    pointIndex = 0;
+                    stallIndex = 0;
+                }
+                out[0].vec[i] = (float) (this.writeEdges.get(edgeIndex).get(pointIndex).x
+                        * (2.0 / scale) - 1.0);
+                out[1].vec[i] = (float) (this.writeEdges.get(edgeIndex).get(pointIndex).y
+                        * (2.0 / scale) - 1.0);
+                out[2].vec[i] = (blank(pointIndex));
+            } catch (Exception e) {
+                // OOB and NPE possible cos of race condition
+                // post("exception:" + e.getMessage());
+                // post(edgeIndex + ": " + pointIndex + ": " + stallIndex);
+                resetIndexes();
+            }
+        }
+    }
+
+    private static final int LAS_OFF = 1;
+    private static final int LAS_ON = 0;
+
+    private float blank(int pointIndex) {
+        return pointIndex == 0 ? LAS_OFF : LAS_ON;
+    }
 }
