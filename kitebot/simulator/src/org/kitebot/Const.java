619f1fe1d2852eb7e2d6dd9afe589a64fc7c1ade
jcalvert <jcalvert@46ce05f2-5dfb-11de-8dc1-e798e446b60a>
2010-06-26 07:47:55 +0000

    Nothing left but to test long-stroke viability.

--- kitebot/simulator/src/org/kitebot/Const.java
+++ kitebot/simulator/src/org/kitebot/Const.java
@@ -2,20 +2,22 @@ package org.kitebot;
 
 public class Const {
 
+	// Gearing //
 	public final static int k = 3;
 	public final static int Ge2s = k + 1;
 	public final static int Gr2s = -k;
 
-	public final static double jg = 1;
-	public final static double je = 1;
-	public final static double jm = 100;
-	
-	public static double planetaryRelation(double planetCarrier,
-			double ringGear) {
+	// returns sun gear position/velocity
+	public static double planetaryRelation(double planetCarrier, double ringGear) {
 		return Ge2s * planetCarrier + Gr2s * ringGear;
 	}
 
-	// Display
+	// Masses //
+	public final static double jg = 1;
+	public final static double je = 1;
+	public final static double jm = 100;
+
+	// Display //
 	public final static int gearDepth = 12;
 
 }
